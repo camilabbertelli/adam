e9f38adf8d12c61648124a2d16e6418ee44aff15
Camila Bacelar Bertelli <camila.bacelar.bertelli@gmail.com>
2024-04-27 15:13:37 +0100

    imp people selection logic

--- src/pages/Dashboard/ImportantPeopleChart.js
+++ src/pages/Dashboard/ImportantPeopleChart.js
@@ -2,6 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "./../../styles/Dashboard/ImportantPeopleChart.css";
 
+import x from "./../../assets/images/dashboard/x.png"
 import info from "./../../assets/images/info-black.png"
 import expand from "./../../assets/images/dashboard/expand.png"
 import shrink from "./../../assets/images/dashboard/shrink.png"
@@ -10,12 +11,22 @@ import { useTranslation } from 'react-i18next';
 
 import * as d3 from "d3"
 
+function noSpaces(str) {
+    if (str)
+        str = str.replace(".", '')
+    if (str)
+        str = str.replace(/\s+/g, '')
+    return str
+}
+
 const ImportantPeopleChart = (props) => {
 
     const { t } = useTranslation()
     const [isExpanded, setIsExpanded] = useState(false)
 
     const [impPeople, setImpPeople] = useState([])
+
+    const [selectedImp, setSelectedImp] = useState([])
 
     function expandImp() {
         document.getElementById("overlay").style.display = (!isExpanded) ? "block" : "none";
@@ -32,28 +43,71 @@ const ImportantPeopleChart = (props) => {
     }, [props.isExpanded])
 
     useEffect(() => {
-        let impData = d3.flatGroup(props.data, d=>d[props.csvIndexes.subject_name])
-        let imp = [...new Set(impData.map(d => d[0]))]
+        let impData = d3.flatGroup(props.data, d => d[props.csvIndexes.subject_name])
+
+        let impAux = {}
+        impData.forEach(entry => {
+            if (entry !== "")
+                impAux[noSpaces(entry[0])] = {
+                    name: entry[0],
+                    entries: entry[1]
+                }
+        })
+
+
+        let sortedkeys = Object.keys(impAux).sort()
+        let imp = {}
+        sortedkeys.forEach((key) => {
+            if (key !== "")
+            imp[key] = impAux[key]
+        })
+
         setImpPeople(imp)
     }, [props.data])
+
+    function removeSelectedImp(index) {
+        let aux = [...selectedImp]
+        aux.splice(index, 1)
+        setSelectedImp(aux)
+    }
+
+    function changeSelected(key) {
+        let entry = impPeople[key]
+
+        let aux = [...selectedImp]
+
+        if (aux.includes(key)) {
+            aux.splice(aux.indexOf(key), 1)
+        } else {
+            if (aux.length === 2)
+                aux.shift()
+            aux.push(key)
+        }
+
+        setSelectedImp(aux)
+    }
 
     return (
         <>
             <div className="imp-people-area shadow">
                 {props.data.length > 0 &&
                     <>
-
                         <div className='imp-left-section'>
-                            {impPeople.map(function (entry) {
+                            {Object.keys(impPeople).map(function (key) {
+                                let entry = impPeople[key]
                                 return (
-                                    <p>{entry}</p>
+                                    <button
+                                        key={key} id={key}
+                                        className={"imp-left-btn " + ((selectedImp.includes(key)) ? "selected-imp" : "")}
+                                        onClick={() => changeSelected(key)}>
+                                        {entry.name}
+                                    </button>
                                 )
                             })}
                         </div>
                         <div className='imp-right-section'>
                             <div className='imp-top-section'>
                                 <div className='imp-title'>
-
                                     <h5 className='imp-top-title'>{t("imp-label")}</h5>
                                     <img alt="info" id="infoImp" src={info}
                                         style={{ marginLeft: "5px", cursor: "pointer" }} width="15px" height="15px"
@@ -65,22 +119,54 @@ const ImportantPeopleChart = (props) => {
                                 />
                             </div>
                             <div className='imp-bottom-section'>
+                                <div className='imp-selected-people'>
+                                    <div className='imp-empty-space'></div>
+                                    <div className='imp-selected-names' style={{borderRight: "1px solid #dddddd"}}>
+                                        <div title={selectedImp.length > 0 ? impPeople[selectedImp[0]].name : t("imp-no-selection-label")}
+                                            className={"default-selection " + (selectedImp.length ? "" : "no-selection")}
+                                            key={selectedImp.length ? selectedImp[0] : "selected1"}
+                                            onClick={() => removeSelectedImp(0)}>
+                                            {selectedImp.length > 0 ? impPeople[selectedImp[0]].name : t("imp-no-selection-label")}
+                                        </div>
+                                        {selectedImp.length > 0 && <img title={t("icon-close")} className={"imp-x"} alt="x" src={x} width="15px" height="15px" onClick={() => removeSelectedImp(0)} />}
+                                    </div>
+                                    <div className='imp-selected-names'>
+                                        <div title={selectedImp.length === 2 ? impPeople[selectedImp[1]].name : t("imp-no-selection-label")}
+                                            className={"default-selection " + (selectedImp.length === 2 ? "" : "no-selection")}
+                                            key={selectedImp.length === 2 ? selectedImp[1] : "selected2"}
+                                            onClick={() => removeSelectedImp(0)}>
+                                            {selectedImp.length === 2 ? impPeople[selectedImp[1]].name : t("imp-no-selection-label")}
+                                        </div>
+                                        {selectedImp.length === 2 && <img title={t("icon-close")} className={"imp-x"} alt="x" src={x} width="15px" height="15px" onClick={() => removeSelectedImp(1)} />}
+                                    </div>
+                                </div>
+                                <div className='imp-selected-section'>
+                                    
+                                </div>
 
                             </div>
                         </div>
                     </>}
                 {props.data.length === 0 && <>
-                    <div className='imp-title'>
+                    <div style={{ display: 'flex', flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "100%" }}>
 
-                        <h5 className='imp-top-title'>{t("imp-label")}</h5>
-                        <img alt="info" id="infoImp" src={info}
-                            style={{ marginLeft: "5px", cursor: "pointer" }} width="15px" height="15px"
-                        />
+                        <div style={{ display: 'flex', flexDirection: "row", justifyContent: "center", width: "100%", height: "15%" }}>
+                            <div className='imp-title'>
+
+                                <h5 className='imp-top-title'>{t("imp-label")}</h5>
+                                <img alt="info" id="infoImp" src={info}
+                                    style={{ marginLeft: "5px", cursor: "pointer" }} width="15px" height="15px"
+                                />
+                            </div>
+                            <img title={isExpanded ? t("icon-shrink") : t("icon-expand")} alt="info" src={isExpanded ? shrink : expand}
+                                style={{ position: "absolute", top: "10px", right: "15px", cursor: "pointer" }} width="15px" height="15px"
+                                onClick={expandImp}
+                            />
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "100%" }}>
+                            {t("no-data-to-show")}
+                        </div>
                     </div>
-                    <img title={isExpanded ? t("icon-shrink") : t("icon-expand")} alt="info" src={isExpanded ? shrink : expand}
-                        style={{ position: "absolute", top: "10px", right: "15px", cursor: "pointer" }} width="15px" height="15px"
-                        onClick={expandImp}
-                    />
                 </>}
             </div>
         </>
